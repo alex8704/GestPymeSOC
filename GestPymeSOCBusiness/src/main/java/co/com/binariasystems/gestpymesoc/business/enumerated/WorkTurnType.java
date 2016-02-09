aaa1f594f4018c79cc4e49aab5f4d3a20ec38953
Alexander Castro Olivo <alexander.castro@techandsolve.com>
2016-02-09 16:16:54 -0500

    Cambio en estructura de proyectos

---
+++ GestPymeSOCBusiness/src/main/java/co/com/binariasystems/gestpymesoc/business/enumerated/WorkTurnType.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package co.com.binariasystems.gestpymesoc.business.enumerated;
+
+/**
+ *
+ * @author Alexander
+ */
+public enum WorkTurnType {
+    DIURNO, NOCTURNO
+}
